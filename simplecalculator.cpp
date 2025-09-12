cc807e8695f835e7fd1d4eb82c0f2d7822398bf8
Anoop Chaudhari <anoopchaudhari263@gmail.com>
2025-09-12 08:01:43 +0000

    simple

---
+++ simplecalculator.cpp
@@ -0,0 +1,43 @@
+
+// Simple Calculator in C++
+#include <iostream>
+using namespace std;
+
+int main() {
+	double num1, num2, result;
+	char op;
+
+	cout << "Enter first number: ";
+	cin >> num1;
+	cout << "Enter second number: ";
+	cin >> num2;
+	cout << "Enter operation (+, -, *, /): ";
+	cin >> op;
+
+	switch(op) {
+		case '+':
+			result = num1 + num2;
+			cout << "Result: " << result << endl;
+			break;
+		case '-':
+			result = num1 - num2;
+			cout << "Result: " << result << endl;
+			break;
+		case '*':
+			result = num1 * num2;
+			cout << "Result: " << result << endl;
+			break;
+		case '/':
+			if(num2 != 0) {
+				result = num1 / num2;
+				cout << "Result: " << result << endl;
+			} else {
+				cout << "Error: Division by zero!" << endl;
+			}
+			break;
+		default:
+			cout << "Invalid operation!" << endl;
+	}
+
+	return 0;
+}
